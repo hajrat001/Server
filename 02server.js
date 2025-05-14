e0f80061914b878633e43b863472a2e579e687a9
hajrat001 <mahiperween940@gmail.com>
2025-05-14 22:58:25 +0530

    Add files via upload

---
+++ 02server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const app = express();
+app.use(express.json({limit: "500mb"}));
+app.use(cors());
+
+app.get('/shama', (req, res) => {
+  const f = "data/shama.json";
+  const r = fs.readFileSync(f, 'utf8');
+  res.send(r);
+})
+app.get('/saba', (req, res) => {
+  const f = "data/saba.json";
+  const r = fs.readFileSync(f, 'utf8');
+  res.send(r);
+})
+app.get('/01data', (req, res) => {
+  const f = "data/01data.json";
+  const r = fs.readFileSync(f, 'utf8');
+  res.send(r);
+})
+app.post('/01dataP', (req, res) => {
+  const f = "data/01data.json";
+  const d = req.body;
+  let old = [];
+  try { old = JSON.parse(fs.readFileSync(f, 'utf8')) } catch {}
+  fs.writeFile(f, JSON.stringify([...old, ...d], null, 2), err => {
+    if (err) return res.status(500).send('fail');
+    res.send('done');
+  });
+});
+  
+
+
+app.listen(3000, () => {console.log('Hellow Ali')});
