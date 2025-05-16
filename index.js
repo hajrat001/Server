f19f32c9a0c68a50bfaca4a30080dc43e60561a5
hajrat001 <mahiperween940@gmail.com>
2025-05-17 01:41:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,34 +6,6 @@ const app = express();
 app.use(express.json({limit: "500mb"}));
 app.use(cors());
 
-app.get('/', (req, res) => {
-  const f = "camera.html";
-  const r = fs.readFileSync(f, 'utf8');
-  res.send(r);
-})
-app.get('/shama', (req, res) => {
-  const f = "data/shama.json";
-  const r = fs.readFileSync(f, 'utf8');
-  res.send(r);
-})
-app.get('/saba', (req, res) => {
-  const f = "data/saba.json";
-  const r = fs.readFileSync(f, 'utf8');
-  res.send(r);
-})
-app.get('/01data', (req, res) => {
-  const f = "data/01data.json";
-  const r = fs.readFileSync(f, 'utf8');
-  res.send(r);
-})
-app.get('/02data', (req, res) => {
-  const f = "data/02data.json";
-  const r = fs.readFileSync(f, 'utf8');
-  res.send(r);
-})
-
-
-
 
 app.post('/01dataP', (req, res) => {
   const f = "data/01data.json";
